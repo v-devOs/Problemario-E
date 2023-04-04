1e62802a2e36524b4939e902829b700353fb8ddc
Uriel Emiliano Galindo Lopez <ugalu448@gmail.com>
2023-04-04 15:03:42 -0600

    Pila de datos terminada

--- p402.java
+++ p402.java
@@ -1,7 +1,23 @@
+import java.util.Scanner;
+
 public class p402 {
   public static void main(String[] args) {
-    
-    int contador, casosPrueba;
+    Scanner input = new Scanner(System.in);
+    Procesador procesador = new Procesador();
+
+    try {
+      int contador, casosPrueba;
+
+      casosPrueba = input.nextInt();
+
+      for (contador = 0;contador < casosPrueba;contador++) {
+        
+      }
+      
+      input.close();
+    } catch (Exception e) {
+      input.close();
+    }
   }
 }
 
@@ -10,14 +26,39 @@ class Procesador{
 }
 
 class pila{
+  Nodo tope;
 
+  public void input(String entrada){
+    Nodo nodoEntrada = new Nodo(entrada);
+
+    if( tope == null )
+      tope = nodoEntrada;
+    else{
+      nodoEntrada.sig = tope;
+      tope = nodoEntrada;
+    }
+  }
+
+  public Nodo output(){
+    if( tope == null )
+      return null;
+    else{
+      Nodo aux = tope;
+      tope = tope.sig;
+      return aux;
+    }
+  }
+
+  pila(){
+    tope = null;
+  }
 }
 
-class nodo{
+class Nodo{
   String valor;
-  nodo sig;
+  Nodo sig;
 
-  nodo(String valor){
+  Nodo(String valor){
     this.valor = valor;
   }
 }
