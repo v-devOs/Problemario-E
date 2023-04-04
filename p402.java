9af8826fd90bc8739536132d4146521592587dc6
Uriel Emiliano Galindo Lopez <ugalu448@gmail.com>
2023-04-04 14:55:40 -0600

    Configuracion incial de p402

---
+++ p402.java
@@ -0,0 +1,23 @@
+public class p402 {
+  public static void main(String[] args) {
+    
+    int contador, casosPrueba;
+  }
+}
+
+class Procesador{
+
+}
+
+class pila{
+
+}
+
+class nodo{
+  String valor;
+  nodo sig;
+
+  nodo(String valor){
+    this.valor = valor;
+  }
+}
